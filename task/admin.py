9e1cb2cff374a748c9c98a15bb2e371ed9b6ba32
Adetomi <ribigbetomi@gmail.com>
2023-08-16 22:00:37 +0100

    commit

---
+++ task/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+
+from .models import Task
+
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'completed')
+
+
+# Register your models here.
+admin.site.register(Task, TodoAdmin)
